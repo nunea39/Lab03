61f5c408d996e9cc0527b3b0c0983b94090743c3
domin <domin@Andrys-PC>
2022-02-17 22:59:27 -0500

    final commit

--- src/main/java/com/mycompany/lab03/Task.java
+++ src/main/java/com/mycompany/lab03/Task.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.lab03;
 
 import java.text.ParseException;
@@ -11,8 +7,9 @@ import java.time.Instant;
 import java.util.Scanner;
 
 /**
+ * Classwork
  *
- * @author soblab
+ * @author Andry Nunez
  */
 public class Task {
     
